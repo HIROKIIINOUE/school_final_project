5ec95c6f23bed60b92c48eb9bcbbf5e82241b5a5
Takaki Hashimoto <takakihashimotogoo@gmail.com>
2026-07-07 22:41:11 -0700

    add protected route

---
+++ frontend/app/(protected)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ProtectedLayout = () => {
+  return (
+    <View>
+      <Text>ProtectedLayout</Text>
+    </View>
+  );
+};
+
+export default ProtectedLayout;
