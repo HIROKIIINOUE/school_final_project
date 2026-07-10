ee98864359db8e03d3d7afc0fa6540c9069d1a03
Takaki Hashimoto <takakihashimotogoo@gmail.com>
2026-07-10 05:51:45 -0700

    feat: implement getMyRooms function to retrieve user's joined rooms

--- backend/src/models/tripRoom.model.ts
+++ backend/src/models/tripRoom.model.ts
@@ -1,5 +1,20 @@
 import { prisma } from "../lib/prisma";
 
-async function gerMyRooms() {
-    await prisma.
+async function getMyRooms(userId: string) {
+  // get all user's joined rooms
+  prisma.tripMember.findMany({
+    where: { userId },
+    select: {
+      role: true,
+      trip: {
+        select: {
+          id: true,
+          title: true,
+          _count: { select: { members: true } }, // It is counting how many matching trip id appear in trip_members table
+        },
+      },
+    },
+  });
 }
+
+export { getMyRooms };
