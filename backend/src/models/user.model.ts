4c37d68676643fecd10409182425139947e349e5
Hiroki Inoue <tyynh.hiroki@gmail.com>
2026-07-16 15:03:51 -0700

    feat : implement Find and Add Profile operation

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,30 @@
+import { Profile } from "../generated/prisma/client";
+import { prisma } from "../lib/prisma";
+import { ProfileInputType } from "../schemas/user.schema";
+
+const findProfile = async (userId: string) => {
+  const profile = await prisma.profile.findUnique({
+    where: { userId },
+  });
+  if (!profile) {
+    return null;
+  }
+  return profile;
+};
+
+const createProfile = async (
+  userId: string,
+  inputData: ProfileInputType,
+): Promise<Profile> => {
+  return await prisma.profile.create({
+    data: {
+      ...inputData,
+      userId,
+    },
+  });
+};
+
+export default {
+  findProfile,
+  createProfile,
+};
