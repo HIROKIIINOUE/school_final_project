15d507150023a9e2cb1251d32f67acdc8130da9a
Takaki Hashimoto <takakihashimotogoo@gmail.com>
2026-07-28 04:25:04 -0700

    feat: add navigation link and back button to OverviewThumbnail component

--- frontend/features/overview/components/OverviewThumbnail.tsx
+++ frontend/features/overview/components/OverviewThumbnail.tsx
@@ -1,6 +1,8 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import { Image } from "expo-image";
+import { Link } from "expo-router";
+import { ChevronLeft } from "lucide-react-native";
 
 const OverviewThumbnail = () => {
   return (
@@ -11,6 +13,11 @@ const OverviewThumbnail = () => {
         style={StyleSheet.absoluteFill}
         contentFit="cover"
       />
+      <Link href="/(protected)" asChild>
+        <Pressable className="absolute left-sm top-sm h-15 w-15 items-center justify-center rounded-full bg-white/50">
+          <ChevronLeft size={32} />
+        </Pressable>
+      </Link>
     </View>
   );
 };
