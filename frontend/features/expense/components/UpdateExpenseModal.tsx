59c8fc6b02b08601b027863e0659b17c9775c85b
Takaki Hashimoto <takakihashimotogoo@gmail.com>
2026-08-10 19:10:39 -0700

    refactor: simplify UpdateExpenseModal component structure

--- frontend/features/expense/components/UpdateExpenseModal.tsx
+++ frontend/features/expense/components/UpdateExpenseModal.tsx
@@ -1,8 +1,5 @@
 import AddExpenseModal from "./AddExpenseModal";
-import {
-  CreateExpenseInput,
-  ExpenseTripMember,
-} from "../types/expense.type";
+import { CreateExpenseInput, ExpenseTripMember } from "../types/expense.type";
 
 type Props = {
   visible: boolean;
@@ -11,12 +8,7 @@ type Props = {
   onUpdate: (input: CreateExpenseInput) => Promise<void>;
 };
 
-const UpdateExpenseModal = ({
-  visible,
-  members,
-  onClose,
-  onUpdate,
-}: Props) => (
+const UpdateExpenseModal = ({ visible, members, onClose, onUpdate }: Props) => (
   <AddExpenseModal
     visible={visible}
     members={members}
