3cf84517ebf45eb52c3e0fc524ef01577f298c59
Takaki Hashimoto <takakihashimotogoo@gmail.com>
2026-08-18 23:59:38 -0700

    implement frontend booking api

--- frontend/features/booking/api/booking.api.ts
+++ frontend/features/booking/api/booking.api.ts
@@ -1,4 +1,12 @@
-import { CreateBookingBody, UpdateBookingBody } from "../types/types";
+import { grabAccessToken } from "@/lib/getAccessToken";
+import {
+  CreateBookingBody,
+  CreatedBooking,
+  DeletedBooking,
+  GetBookings,
+  UpdateBookingBody,
+  UpdatedBooking,
+} from "../types/types";
 
 const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
 
@@ -6,19 +14,69 @@ if (!BACKEND_URL) {
   throw new Error("EXPO_PUBLIC_BACKEND_URL is not configured");
 }
 
-async function getBookings({ tripId }: { tripId: string }) {
-  const res = await fetch(`${BACKEND_URL}/api/trips/${tripId}/bookings`);
+type ApiRes<T> = { data: T };
+
+function generateBookingRoute({
+  tripId,
+  bookingId,
+}: {
+  tripId: string;
+  bookingId?: string;
+}) {
+  const baseUrl = `${BACKEND_URL}/api/trips/${tripId}/bookings`;
+  return bookingId ? `${baseUrl}/${bookingId}` : baseUrl;
 }
 
-async function createBooking({
+async function authorizationHeaders() {
+  const accessToken = await grabAccessToken();
+  return { Authorization: `Bearer ${accessToken}` };
+}
+
+export async function getBookings({ tripId }: { tripId: string }) {
+  const authorization = await authorizationHeaders();
+  const apiRoute = generateBookingRoute({ tripId });
+  const res = await fetch(apiRoute, {
+    method: "GET",
+    headers: { ...authorization },
+    credentials: "include",
+  });
+  const data: ApiRes<GetBookings> = await res.json();
+
+  if (!res.ok) {
+    console.error("res.ok failed. Failed to fetch  bookings", data);
+    throw new Error("Failed to fetch bookings");
+  }
+
+  return data.data.bookings;
+}
+
+export async function createBooking({
   tripId,
   body,
 }: {
   tripId: string;
   body: CreateBookingBody;
-}) {}
+}) {
+  const authorization = await authorizationHeaders();
+  const apiRoute = generateBookingRoute({ tripId });
+  const res = await fetch(apiRoute, {
+    method: "POST",
+    headers: { "Content-Type": "application/json", ...authorization },
+    credentials: "include",
+    body: JSON.stringify(body),
+  });
 
-async function updateBooking({
+  const data: ApiRes<CreatedBooking> = await res.json();
+
+  if (!res.ok) {
+    console.error("res.ok failed. Failed to create  bookings", data);
+    throw new Error("Failed to create bookings");
+  }
+
+  return data.data.booking;
+}
+
+export async function updateBooking({
   tripId,
   bookingId,
   body,
@@ -26,4 +84,46 @@ async function updateBooking({
   tripId: string;
   bookingId: string;
   body: UpdateBookingBody;
-}) {}
+}) {
+  const authorization = await authorizationHeaders();
+  const apiRoute = generateBookingRoute({ tripId, bookingId });
+  const res = await fetch(apiRoute, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json", ...authorization },
+    credentials: "include",
+    body: JSON.stringify(body),
+  });
+
+  const data: ApiRes<UpdatedBooking> = await res.json();
+
+  if (!res.ok) {
+    console.error("res.ok failed. Failed to update  bookings", data);
+    throw new Error("Failed to update bookings");
+  }
+
+  return data.data.updatedBooking;
+}
+
+export async function deleteBooking({
+  tripId,
+  bookingId,
+}: {
+  tripId: string;
+  bookingId: string;
+}) {
+  const authorization = await authorizationHeaders();
+  const apiRoute = generateBookingRoute({ tripId, bookingId });
+  const res = await fetch(apiRoute, {
+    method: "DELETE",
+    headers: { ...authorization },
+  });
+
+  const data: ApiRes<DeletedBooking> = await res.json();
+
+  if (!res.ok) {
+    console.error("Failed to delete booking: ", data);
+    throw new Error("Failed to delete booking");
+  }
+
+  return data.data.deletedBooking;
+}
--- frontend/features/booking/types/types.ts
+++ frontend/features/booking/types/types.ts
@@ -5,28 +5,61 @@ export type FlightDetailsType = {
 };
 
 export type HotelDetailsType = {
-  address?: string;
-  roomType?: string;
-  checkInInstructions?: string;
+  address?: string | null;
+  roomType?: string | null;
+  checkInInstructions?: string | null;
 };
+
 export type CreateBookingBody = {
   type: "HOTEL" | "FLIGHT";
   title: string;
-  provider?: string;
-  confirmationCode?: string;
-  startTime?: Date;
-  endTime?: Date;
-  note?: string;
+  provider?: string | null;
+  confirmationCode?: string | null;
+  startTime?: string | null;
+  endTime?: string | null;
+  note?: string | null;
   details: FlightDetailsType | HotelDetailsType;
 };
 
 export type UpdateBookingBody = {
   type?: "FLIGHT" | "HOTEL";
   title?: string;
-  provider?: string;
-  confirmationCode?: string;
-  startTime?: Date;
-  endTime?: Date;
-  note?: string;
+  provider?: string | null;
+  confirmationCode?: string | null;
+  startTime?: string | null;
+  endTime?: string | null;
+  note?: string | null;
   details?: FlightDetailsType | HotelDetailsType;
 };
+
+export type BookingBase = {
+  id: string;
+  tripId: string;
+  createdById: string;
+  title: string;
+  provider: string | null;
+  confirmationCode: string | null;
+  startTime: string | null;
+  endTime: string | null;
+  note: string | null;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type FlightBooking = BookingBase & {
+  type: "FLIGHT";
+  details: FlightDetailsType;
+};
+export type HotelBooking = BookingBase & {
+  type: "HOTEL";
+  details: HotelDetailsType;
+};
+
+export type Booking = FlightBooking | HotelBooking;
+export type GetBookings = { bookings: Booking[] };
+
+export type CreatedBooking = { booking: Booking };
+
+export type UpdatedBooking = { updatedBooking: Booking };
+
+export type DeletedBooking = { deletedBooking: { bookingId: string } };
